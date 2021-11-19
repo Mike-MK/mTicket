bb18840e1561fe738a8ab9e65285827d1bdbaaed
Mike-MK <michaelmukiri99@gmail.com>
2021-11-19 10:55:50 +0300

    Production db settings

--- config/database.php
+++ config/database.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Str;
 
-return [
+$DATABASE_URL=parse_url("postgres://yyzoxivtxgnznt:bf6276e9a47c9918fc79437620739f90a60822f7a39030911494f30e1ffbf950@ec2-52-54-23-130.compute-1.amazonaws.com:5432/d7tekar5kom3pf");
 
+return [
+    
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => "postgres://yyzoxivtxgnznt:bf6276e9a47c9918fc79437620739f90a60822f7a39030911494f30e1ffbf950@ec2-52-54-23-130.compute-1.amazonaws.com:5432/d7tekar5kom3pf",
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["host"]),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
